1b7e02d580167bedebf194319346634b3ab976b0
shreyahprabhu <95059689+shreyahprabhu@users.noreply.github.com>
2025-09-28 19:25:31 +0530

    Update User.java

--- bookify/src/main/java/com/project/bookify/model/User.java
+++ bookify/src/main/java/com/project/bookify/model/User.java
@@ -13,11 +13,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
     private String email;
     private String password; 
-
     public User() {
     }
 
